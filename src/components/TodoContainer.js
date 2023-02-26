6c0d3190a90ef038e186fffd04e627b835438a7d
Rohan23045 <118437754+Rohan23045@users.noreply.github.com>
2023-02-26 21:15:46 +0530

    Create TodoContainer.js

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import TodoItem from "./TodoItem";
+import { Bars } from "react-loader-spinner";
+
+function TodoContainer({ addedTask }) {
+	const [todos, setTodos] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [newTask, setNewTask] = useState(addedTask);
+	
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/todos")
+			.then((response) => response.json())
+			.then((json) => {
+				setTimeout(() => {
+					setTodos(json);
+					setLoading(true);
+				}, 1000);
+				
+			});
+	}, []);
+
+	// Whenever the new task is added then that task
+	useEffect(() => {
+		let taskAddedByUser = addedTask.map((task, index) => {
+			return <TodoItem task={task} key={index} />;
+		});
+		setNewTask(taskAddedByUser)
+	},  [newTask])
+
+	
+	return (
+		<div className="todo-container">
+			{newTask}
+			{loading ? (
+				todos.map((task, index) => {
+					return <TodoItem task={task} key={index} />;
+				})
+			) : (
+				<Bars height="80" width="80" color="#4fa94d" ariaLabel="bars-loading" visible={true} />
+			)}
+		</div>
+	);
+}
+
+export default TodoContainer;
